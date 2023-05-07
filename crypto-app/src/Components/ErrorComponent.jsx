74f73b33254bd91d47f86cc9a7bf573cf2dba6b4
Abhay Thanak <abhaythanak@gmail.com>
2023-05-07 19:24:01 +0530

    error,exchange,coinsdetails,currency selector componenets added

--- crypto-app/src/Components/ErrorComponent.jsx
+++ crypto-app/src/Components/ErrorComponent.jsx
@@ -1,20 +1,20 @@
-// import { Alert, AlertIcon } from "@chakra-ui/react";
+import { Alert, AlertIcon } from "@chakra-ui/react";
 
 
-//  const ErrorComponent = ({ message }) => {
-//   return (
-//     <Alert
-//       status="error"
-//       position={"fixed"}
-//       bottom={"4"}
-//       left={"50%"}
-//       transform={"translateX(-50%)"}
-//       w={"container.lg"}
-//     >
-//       <AlertIcon />
-//       {message}
-//     </Alert>
-//   );
-// };
+const ErrorComponent = ({ message }) => {
+  return (
+    <Alert
+      status="error"
+      position={"fixed"}
+      bottom={"4"}
+      left={"50%"}
+      transform={"translateX(-50%)"}
+      w={"container.lg"}
+    >
+      <AlertIcon />
+      {message}
+    </Alert>
+  );
+};
 
-// export default ErrorComponent;
+export default ErrorComponent;
