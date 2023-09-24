47600347053bba8c6e4b6eda275d2f6b0b9830b5
Abhay Thanak <abhaythanak@gmail.com>
2023-09-24 19:05:35 +0530

    Register TMDB API & create an app & get access token & Get data from TMDB now playing movies list API

--- flix-mingle/README.md
+++ flix-mingle/README.md
@@ -18,6 +18,12 @@
 - BugFix: if the user is not logged in Redirect /browse to login page and vice-versa
 - unsubscribe to the on AuthStateChange callback
 - Add hard coded value in constants value
+- Register TMDB API & create an app & get access token
+- Get data from TMDB now playing movies list API
+-
+-
+-
+-
 -
 
 # Feature
--- flix-mingle/src/components/Browse/Browse.jsx
+++ flix-mingle/src/components/Browse/Browse.jsx
@@ -1,6 +1,20 @@
+import { useEffect } from "react";
+import { API_OPTIONS } from "../../utils/constants";
 import Header from "../Header/Header";
 
 export default function Browse(params) {
+    const getNowPlayingMovies = async () => {
+        const data = await fetch(
+            "https://api.themoviedb.org/3/movie/now_playing?page=1",
+            API_OPTIONS
+        );
+        const json = await data.json();
+        console.log(json)
+    };
+
+    useEffect(()=>{
+        getNowPlayingMovies()
+    },[])
     return(
         <div className="">
             <Header/>
