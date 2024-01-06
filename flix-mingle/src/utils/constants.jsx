7af97e0498edc0fe598fbac82783223fbdafe421
Abhay Thanak <abhaythanak@gmail.com>
2024-01-06 22:08:50 +0530

    update api

--- flix-mingle/src/utils/constants.jsx
+++ flix-mingle/src/utils/constants.jsx
@@ -4,15 +4,21 @@ export const LOGO =
 export const USER_AVATAR =
   "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
 
+// export const API_OPTIONS = {
+//     method: 'GET',
+//     headers: {
+//       accept: 'application/json',
+//       Authorization:` Bearer ` + process.env.REACT_APP_TMDB_KEY,
+//     },
+// };
+
 export const API_OPTIONS = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization:` Bearer ` + process.env.REACT_APP_TMDB_KEY,
-    },
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMWI0ZTFiZWIwOTlmNzU2ZDBkM2IwZDcyZjYyNmFjYyIsInN1YiI6IjY1MGZmM2ZjMjZkYWMxMDBhYzA4OTllNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8KIdbw8ge_lS7WgOVagCA1fFcYBDMC0iJAV-cEUr5ek'
+  }
 };
-
-
 
 export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
 
